c00ed79194d9fd262e111870b3faf25d4d917a77
莫瑞益 <moruiyi043@126.com>
2017-04-08 17:05:10 +0800

    17:05

---
+++ Parking.Web/Areas/SystemManager/Controllers/FaultSummaryController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Parking.Auxiliary;
+using Parking.Data;
+using Parking.Core;
+
+namespace Parking.Web.Areas.SystemManager.Controllers
+{
+    public class FaultSummaryController : Controller
+    {
+        // GET: SystemManager/FaultSummary
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetDeviceList()
+        {
+            List<Device> devlst = new CWDevice().FindList(dev => true);
+            return Json(devlst,JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
